8d503096c834fa376c2efe486305741961c052a0
starkweb108 <starkweb2@gmail.com>
2022-11-05 10:01:12 +0100

    error fixes

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const limiter = rateLimit({
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
-
+app.set("trust proxy", 1);
 app.use(limiter);
 app.use(cors());
 app.use(cookieParser(process.env.JWT_SECRET));
